77f45d6c8c0a9dc931c7a67da370ef036475aad4
jschomchoey <tpcc.general@gmail.com>
2026-01-24 23:15:38 +0700

    update: prepare page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { use } from "react";
 
-import HeroSection from "@/components/sections/hero/hero";
+import HeroSection from "@/components/sections/hero/Hero";
 import PostsSection from "@/components/sections/posts/posts.server";
 
 export default function Home() {
