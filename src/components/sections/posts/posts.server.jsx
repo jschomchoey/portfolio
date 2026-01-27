2f6f6cb79212be8cf6e2649f14eedd4ddf75a132
jschomchoey <tpcc.general@gmail.com>
2026-01-27 13:16:49 +0700

    update: post detail

--- src/components/sections/posts/posts.server.jsx
+++ src/components/sections/posts/posts.server.jsx
@@ -15,16 +15,32 @@ export default async function PostsSection() {
       userId: 1,
       id: 1,
       slug: "framer-motion-with-nextjs-13",
-      image: "/images/posts/clicknext_logo.jpg",
+      image: "/images/posts/next_motion.jpg",
       title: "เมื่อ Framer Motion ต้องการ Client Component ร่วมกับ Next.js 13",
       description:
         "เลือกใช้ Next.js เพื่อจะใช้ประโยชน์จาก Server Component แต่ Framer Motion ดันต้องการ Client Component ในการทำ Animation จะทำอย่างไรดี?",
       body: "เลือกใช้ Next.js เพื่อจะใช้ประโยชน์จาก Server Component แต่ Framer Motion ดันต้องการ Client Component ในการทำ Animation จะทำอย่างไรดี?",
       date: "08 Jan 2026",
     },
+
     {
       userId: 1,
       id: 2,
+      image: "/images/posts/CrossBoot.png",
+      slug: "jsonplaceholder-fake-online-rest-api",
+      title: "เมื่อ Pain Point ตัวเอง กลายมาเป็น CrossBoot Open Source",
+      description:
+        "macOS Arm ดันไม่มี Bootcamp ให้สร้าง USB ติดตั้ง Windows ไหนลองดู GUI เจ้าตลาดซิ เอ้า! ดันไม่รองรับอีก สร้างใช้เองเลยละกัน",
+      body:
+        "et iusto sed quo iure\n" +
+        "voluptatem occaecati omnis eligendi aut ad\n" +
+        "voluptatem doloribus vel accusantium quis pariatur\n" +
+        "molestiae porro eius odio et labore et velit aut",
+      date: "08 Jan 2026",
+    },
+    {
+      userId: 1,
+      id: 3,
       slug: "internship-review-clicknext",
       image: "/images/posts/clicknext_logo.jpg",
       title: "รีวิวฝึกงานที่ ClickNext - Frontend Developer",
@@ -35,21 +51,6 @@ export default async function PostsSection() {
         "sequi sint nihil reprehenderit dolor beatae ea dolores neque\n" +
         "fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\n" +
         "qui aperiam non debitis possimus qui neque nisi nulla",
-      date: "08 Jan 2026",
-    },
-    {
-      userId: 1,
-      id: 3,
-      image: "/images/posts/clicknext_logo.jpg",
-      slug: "jsonplaceholder-fake-online-rest-api",
-      title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-      description:
-        "เลือกใช้ Next.js เพื่อจะใช้ประโยชน์จาก Server Component แต่ Framer Motion ดันต้องการ Client Component ในการทำ Animation จะทำอย่างไรดี?",
-      body:
-        "et iusto sed quo iure\n" +
-        "voluptatem occaecati omnis eligendi aut ad\n" +
-        "voluptatem doloribus vel accusantium quis pariatur\n" +
-        "molestiae porro eius odio et labore et velit aut",
       date: "08 Jan 2026",
     },
   ];
--- src/components/sections/projects/projects.server.jsx
+++ src/components/sections/projects/projects.server.jsx
@@ -18,7 +18,7 @@ export default async function ProjectsSection() {
       image: "/images/projects/clicknext_logo.jpg",
       title: "Clicknext | Internship / Frontend Developer",
       description:
-        "Developed responsive web pages and content layouts based on design mockups, ensuring cross-device compatibility.",
+        "Worked on corporate CMS customization, security vulnerability fixes, and government backend testing, focusing on responsive design, system stability, and bug reduction.",
       body: "In sit amet metus a ipsum eleifend dictum vitae vel erat. Sed tincidunt sapien massa, tempor mattis lacus pellentesque vitae. Donec congue varius enim sit amet condimentum. Curabitur sodales erat ut mi gravida, at elementum nulla efficitur. Nam dolor quam, elementum sit amet tincidunt ac,",
       date: "08 Jan 2026",
       type: "Internship",
@@ -26,11 +26,11 @@ export default async function ProjectsSection() {
     {
       userId: 1,
       id: 2,
-      slug: "project-find-mate-roommate-recommendation-full-stack",
-      image: "/images/projects/CrossBoot.png",
-      title: "Project: Find Mate - Roomate Recommendation / Full Stack",
+      slug: "project-find-mate-roommate-recommendation",
+      image: "/images/projects/FindMate.webp",
+      title: "Project: Find Mate - Roomate Recommendation",
       description:
-        "ประสบการณ์การฝึกงานที่ ClickNext ในตำแหน่ง Frontend Developer ประสบการณ์ไม่มีวันลืม เป็นอย่างไร มีอะไรได้เรียนรู้บ้าง มาดูกัน!",
+        "Built a full-stack roommate recommendation application using personality-based compatibility matching. Implemented a secure mutual match system with OTP verification and an admin dashboard for user and report management.",
       body:
         "est rerum tempore vitae\n" +
         "sequi sint nihil reprehenderit dolor beatae ea dolores neque\n" +
@@ -58,11 +58,11 @@ export default async function ProjectsSection() {
     {
       userId: 1,
       id: 4,
-      image: "/images/projects/CrossBoot.png",
-      slug: "crossboot-windows-bootable-usb-creator-on-macos",
-      title: "CrossBoot - Windows Bootable USB Creator on macOS",
+      image: "/images/projects/CowayCorp.png",
+      slug: "coway-sales-representative-websites",
+      title: "Coway Sales Representative Websites",
       description:
-        "Architected a macOS utility that simplifies creating Windows bootable USBs with Legacy BIOS, UEFI, and Secure Boot support, featuring automatic WIM splitting and Windows 11 bypass capabilities.",
+        "Designed and launched sales representative websites using Wix, focusing on user-centric UI/UX, SEO optimization, and dynamic content management to improve visibility and engagement.",
       body:
         "et iusto sed quo iure\n" +
         "voluptatem occaecati omnis eligendi aut ad\n" +
